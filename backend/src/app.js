924c366457d11a71b41d89d34b2cd08b8ab2d421
Amol Rai <45944308+AmolRai@users.noreply.github.com>
2024-02-17 20:43:16 +0530

    fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,6 +16,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 // app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
