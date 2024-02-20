ca43046751c6ddc48e08102709f52a0f5f1170ca
Amol Rai <45944308+AmolRai@users.noreply.github.com>
2024-02-20 17:29:06 +0530

    fixed

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -40,7 +40,9 @@ export const AuthProvider = ({ children }) => {
     const d = new Date();
     d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
     let expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    // Set SameSite attribute to None for cross-site requests
+    document.cookie =
+      cname + "=" + cvalue + ";" + expires + ";path=/;SameSite=None;Secure";
   }
 
   const logout = async () => {
