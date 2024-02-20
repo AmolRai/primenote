3efa9c4344a58c6d376ff728935b93441ce244b0
Amol Rai <45944308+AmolRai@users.noreply.github.com>
2024-02-20 18:10:56 +0530

    fixed

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -91,13 +91,15 @@ const login = async (req, res) => {
       throw new ApiError(404, "User doesn't exists");
     }
 
-    res.cookie("jwt", token, {
+    const cookieOptions = {
       maxAge: new Date(
         Date.now() + process.env.JWT_COOKIE_EXPIR_IN * 24 * 60 * 60 * 1000
       ),
       secure: true, // It is for https not for http
       httpOnly: true,
-    });
+    };
+
+    res.cookie("jwt", token, cookieOptions);
 
     return res
       .status(200)
